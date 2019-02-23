b2ad1e4437f3c14cf8d97b7fc20537aedf7bfcc1
Vincent Schoener <vincent.schoener@gmail.com>
2019-02-23 20:04:23 +0100

    Fix circleci (#2)

--- src/Container/Container.ts
+++ src/Container/Container.ts
@@ -3,16 +3,25 @@ import { NotFoundExceptionInterface } from './NotFoundExceptionInterface';
 import { ServiceNotEligibleException } from '../Exception/ServiceNotEligibleException';
 
 export class Container<T> implements ContainerInterface {
+  /** Contains the services */
   private services: Map<string, T>;
 
   constructor() {
     this.services = new Map<string, T>();
   }
 
+  /**
+   * Check if the service exist in the container
+   * @param id
+   */
   public has(id: string): boolean {
     return this.services.has(id);
   }
 
+  /**
+   * Get the service from the provided id
+   * @param id
+   */
   public get(id: string): any {
     if (!this.has(id)) {
       throw new NotFoundExceptionInterface('Container does not exist');
@@ -24,7 +33,7 @@ export class Container<T> implements ContainerInterface {
   /**
    * Set the service
    * @param id
-   * @param service
+   * @param service should be an object / instance of a class
    */
   public set(id: string, service: T) {
     if (service && typeof service !== 'object') {
